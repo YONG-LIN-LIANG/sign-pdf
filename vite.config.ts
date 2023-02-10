ac3544c6457171b0d967ad30c31ffb320064760d
Steven <steven@gmail.com>
2023-02-10 21:57:27 +0800

    fix: network setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'sign-pdf',
   plugins: [react()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
     ]
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5173
+  // }
 })
